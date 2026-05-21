288bf6d486a3d88112ea7ea6c61eb088bb846b1f
Dinesh <shinde124dinu@gmail.com>
2026-05-21 14:21:22 +0530

    My sample

--- main.tf
+++ main.tf
@@ -12,6 +12,14 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "dinesh-rg"
+  name     = "dinesh-rg1"
   location = "Central India"
-} 
+}
+
+resource "azurerm_storage_account" "store01" {
+  name                     = "dineshstore01"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
